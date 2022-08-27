69f7fc51a02f5bd34e01e9aadf68ef22cbc61abd
sid-tiwari19 <siddharth19402@gmail.com>
2022-08-27 20:32:04 +0530

    base condition

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,5 +1,7 @@
-def factorial(int x):
-    
+def factorial(x):
+    if x==0 or x==1:
+        return 1
 
-x=int(input("Enter the number whose factorial is to be found"))
+
+x=int(input("Enter the number whose factorial is to be found: "))
 factorial(x)
