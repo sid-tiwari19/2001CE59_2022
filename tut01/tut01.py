7a60248ed8a02cb0a8f3e1f4a1c3dd0c227bd510
sid-tiwari19 <siddharth19402@gmail.com>
2022-09-12 22:24:19 +0530

    making a new column and inserting the mean values of the data

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -7,6 +7,12 @@ import pandas as pd
 #reading the input file
 df = pd.read_csv("octant_input.csv") 
 
+#data pre-prcessing:
+df.at[0,'u_avg']=df['U'].mean() 
+df.at[0,'v_avg']=df['V'].mean()
+df.at[0,'w_avg']=df['W'].mean()
+
+
 
 mod=5000
 octact_identification(mod)
