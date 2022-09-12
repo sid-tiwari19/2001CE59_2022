8c96b2c280d51f94a31949bde24641799931e733
sid-tiwari19 <siddharth19402@gmail.com>
2022-09-12 22:55:54 +0530

    categorizing the data into octaves

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,3 +1,29 @@
+#function for categorizing the data into different octant 
+def oct(x,y,z):
+    if x>0:
+        if y>0:
+            if z>0:
+                return 1
+            else :
+                return -1
+        else:
+            if z>0:
+                return 4
+            else :
+                return -4
+    else:
+        if y>0:
+            if z>0:
+                return 2
+            else :
+                return -2
+        else:
+            if z>0:
+                return 3
+            else :
+                return -3
+
+
 def octact_identification(mod=5000):
     print('a')
 
@@ -16,7 +42,8 @@ df['U-u_avg']=df['U']-df.at[0,'u_avg']
 df['V-v_avg']=df['V']-df.at[0,'v_avg']
 df['W-w_avg']=df['W']-df.at[0,'w_avg']
 
-
+#applying the function made to categorize the data using .apply function
+df['octant'] = df.apply(lambda x: oct(x['U-u_avg'], x['V-v_avg'], x['W-w_avg']),axis=1)
 
 
 
