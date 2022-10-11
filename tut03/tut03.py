082bace43217b1d5a19f704ef4ae07bef4149828
sid-tiwari19 <siddharth19402@gmail.com>
2022-10-11 19:40:46 +0530

    octant function as done before

--- tut03/tut03.py
+++ tut03/tut03.py
@@ -1,10 +1,35 @@
-#Siddharth Tiwari 2001CE59
-import openpyxl
-wb = openpyxl.load_workbook(r'input_octant_longest_subsequence.xlsx')
-st = wb.active
-rcount=st.max_row
-total_count=rcount-1
+#SIDDHARTH TIWARI 2001CE59
 
-#List storing octant signs
-lis = [1, -1, 2, -2, 3, -3, 4, -4]
+#importing pandas and numpy
+import pandas as pd 
+import numpy as np
+import os
+
+os.system('cls')
+
+#function for categorizing the data into different octant 
+def oct(x,y,z):
+    if x>0:
+        if y>0:
+            if z>0: 
+                return 1 #when x,y,z all are positive
+            else :
+                return -1 #when x,y is positive and z is negative
+        else:
+            if z>0:
+                return 4  #when x,z is positive and y is negative
+            else :
+                return -4 #when z,y is negative and x is positive
+    else:
+        if y>0:
+            if z>0:
+                return 2 #when z,y is positive and x is negative
+            else :
+                return -2 #when z,x is negative and y is positive
+        else:
+            if z>0:
+                return 3 #when x,y is negative and z is positive
+            else :
+                return -3 #when x,y,z all are negative
+
 
