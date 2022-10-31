24b1185a233d8137739d453cbce8db6432c9da7f
sid-tiwari19 <siddharth19402@gmail.com>
2022-10-31 18:20:30 +0530

    applying the octant function

--- tut05/tut05.py
+++ tut05/tut05.py
@@ -48,3 +48,54 @@ df['W-w_avg']=df['W']-df.at[0,'w_avg']
 #applying the function made to categorize the data using .apply function
 df['octant'] = df.apply(lambda x: oct(x['U-u_avg'], x['V-v_avg'], x['W-w_avg']),axis=1)
 
+#leaving an empty column
+df[' '] = ''
+df.at[1,' '] = 'user input'
+
+#counting overall using value_counts function
+df.at[0,'octant ID'] = 'overall count'
+df.at[0,'1']  = df['octant'].value_counts()[1]
+df.at[0,'-1'] = df['octant'].value_counts()[-1]
+df.at[0,'2']  = df['octant'].value_counts()[2]
+df.at[0,'-2'] = df['octant'].value_counts()[-2]
+df.at[0,'3'] = df['octant'].value_counts()[3]
+df.at[0,'-3'] = df['octant'].value_counts()[-3]
+df.at[0,'4'] = df['octant'].value_counts()[4]
+df.at[0,'-4'] = df['octant'].value_counts()[-4]
+
+#asking user for input
+mod = int(input("Enter a value: "))
+df.at[1,'octant ID']=mod
+
+size = len(df['octant'])
+i=0
+#using a while loop to split the data in the given range
+while(size):
+    temp = mod
+    
+    #for last range we have to take only till last value
+    if size<mod:
+        mod = size
+    x = i*temp
+    y = i*temp+mod - 1
+    
+    #inserting range and their corresponding data
+    df.at[i+2,'octant ID'] = str(x) +'-'+ str(y) 
+    
+    #making a new dataframe for a choosen range to count octants
+    df1 = df.loc[x:y] 
+
+    #counting octant for the taken range and inserting in the cell
+    df.at[i+2,'-1'] = df1['octant'].value_counts()[-1]
+    df.at[i+2,'1']  = df1['octant'].value_counts()[1]
+    df.at[i+2,'-2'] = df1['octant'].value_counts()[-2]
+    df.at[i+2,'2']  = df1['octant'].value_counts()[2]
+    df.at[i+2,'-3'] = df1['octant'].value_counts()[-3]
+    df.at[i+2,'3'] = df1['octant'].value_counts()[3]
+    df.at[i+2,'-4'] = df1['octant'].value_counts()[-4]
+    df.at[i+2,'4'] = df1['octant'].value_counts()[4]
+
+    i = i + 1
+    size = size - mod
+
+
