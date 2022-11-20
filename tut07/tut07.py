376400047bd686a0e936608b1cbdd671acd5c98d
sid-tiwari19 <siddharth19402@gmail.com>
2022-11-20 18:56:48 +0530

    importing the necessary libraries

--- tut07/tut07.py
+++ tut07/tut07.py
@@ -1,34 +1,14 @@
-
+#SIDDHARTH TIWARI 2001CE59
 from datetime import datetime
 start_time = datetime.now()
-
-#Help
-def octant_analysis(mod=5000):
-	pass
-
-##Read all the excel files in a batch format from the input/ folder. Only xlsx to be allowed
-##Save all the excel files in a the output/ folder. Only xlsx to be allowed
-## output filename = input_filename[_octant_analysis_mod_5000].xlsx , ie, append _octant_analysis_mod_5000 to the original filename. 
-
-###Code
-
-from platform import python_version
-ver = python_version()
-
-if ver == "3.8.10":
-	print("Correct Version Installed")
-else:
-	print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
-
-
-mod=5000
-octant_analysis(mod)
+import openpyxl
+from openpyxl.styles import Color, PatternFill, Font, Border, Side
+import glob
+import os
+os.system("cls")
 
 
 
 
 
 
-#This shall be the last lines of the code.
-end_time = datetime.now()
-print('Duration of Program Execution: {}'.format(end_time - start_time))
