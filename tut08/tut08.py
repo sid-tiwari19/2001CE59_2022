d77f96ba65a4f41dc53451936faf1b9c95b46dd1
sid-tiwari19 <siddharth19402@gmail.com>
2022-11-20 17:14:51 +0530

    indexing and using regex

--- tut08/tut08.py
+++ tut08/tut08.py
@@ -18,4 +18,54 @@ def one_inn(inn1,bat_pl,bow_pl,s): #making function for one innings
     s3 = innfow.active
     s1.column_dimensions['A'].width = 25
 
+    #scorecard and index
+    s1['A1'] = s + ' Innings'
+    s1['I1'] = '0-0'
+    s1['J1'] = '0 overs'
+    s1['A2'] = 'Batter'
+    s1['F2'] = 'R'
+    s1['G2'] = 'B'
+    s1['H2'] = '4s'
+    s1['I2'] = '6s'
+    s1['J2'] = 'SR'
+
+    s2['A1'] = 'Bowler'
+    s2['D1'] = 'O'
+    s2['E1'] = 'M'
+    s2['F1'] = 'R'
+    s2['G1'] = 'W'
+    s2['H1'] = 'NB'
+    s2['I1'] = 'WD'
+    s2['J1'] = 'ECO'
+
+    s3['A1'] = 'Fall of wickets'
+    
+    #using regex
+    over = re.compile(r'(\d\d?\.\d)')
+    zero = re.compile(r'no run')
+    no_ball = re.compile(r', (no ball),')
+    wide = re.compile(r', wide,')
+    wide2 = re.compile(r', 2 wides,')
+    wide3 = re.compile(r', 3 wides,')
+    single = re.compile(r', 1 run,')
+    six = re.compile(r', SIX,')
+    four = re.compile(r', FOUR,')
+    byes = re.compile(r', (byes),')
+    lbyes = re.compile(r', (leg byes),')
+    double = re.compile(r', 2 runs,')
+    triple = re.compile(r', 3 runs,')
+    out = re.compile(r', out')
+    player = re.compile(r'(\d\d?\.\d) (\w+) (to|\w+ to) (\w+)( \w+)?,')
+    caught = re.compile(r', out Caught by (\w+)')
+    lbw = re.compile(r', out Lbw!!')
+    bowled = re.compile(r', out Bowled!!')
+    run_out = re.compile(r'Run Out!! ')
+    runs = 0
+    wickets = 0
+    nb = 0
+    nlb = 0
+    nw = 0
+    nnb = 0
+    ppr = 0
+
     
