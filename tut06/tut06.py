82f6b58f764f5d841f5db1681cb07c4e9f0b34e0
sid-tiwari19 <siddharth19402@gmail.com>
2022-11-20 19:24:52 +0530

    creating a function for report

--- tut06/tut06.py
+++ tut06/tut06.py
@@ -7,3 +7,21 @@ start_time = datetime.now()
 
 os.system('cls')
 
+def AR():
+
+    try:
+  
+        DataF =pd.read_csv('input_attendance.csv')
+        DataF2 =pd.read_csv('input_registered_students.csv')
+    except:
+        print("Errorin reading the file.")
+
+    # Adding empty columns 
+    DataF['Roll'] = ''
+    DataF['Time'] =''
+    DataF['Date'] = ''
+    DataF['Day'] = ''
+    DataF['Month'] =''
+    DataF['Year'] = ''
+
+ 
