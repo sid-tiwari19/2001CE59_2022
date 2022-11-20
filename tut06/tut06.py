67765258a82ee4b80fb1752ab3005c5677ae9880
sid-tiwari19 <siddharth19402@gmail.com>
2022-11-20 19:25:43 +0530

    concating the given data

--- tut06/tut06.py
+++ tut06/tut06.py
@@ -24,4 +24,29 @@ def AR():
     DataF['Month'] =''
     DataF['Year'] = ''
 
- 
+    # Concatenating the given data 
+    DataF.loc[DataF.Roll == '', 'Roll'] = DataF.Attendance.str.split().str.get(0)
+    DataF.loc[DataF.Time == '', 'Time'] = DataF.Timestamp.str.split().str.get(1)
+    DataF.loc[DataF.Date == '', 'Date'] = DataF.Timestamp.str.split().str.get(0)
+    DataF.loc[DataF.Day == '', 'Day'] = DataF.Date.str.split('-').str.get(0)
+    DataF.loc[DataF.Month == '', 'Month'] = DataF.Date.str.split('-').str.get(1)
+    DataF.loc[DataF.Year == '', 'Year'] = DataF.Date.str.split('-').str.get(2)
+
+    # (Modays and Thurdays)
+    v_D = []
+    Dhwaj = ''
+    
+    # Indexing over the dataframe for getting valid dates, using 'datetime' library for the same.
+    for i in DataF.index:
+        Dhatt = datetime(int(DataF['Year'][i]), int(DataF['Month'][i]), int(DataF['Day'][i]))
+        if (Dhatt.weekday() == 0 or Dhatt.weekday() == 3):
+            if (Dhwaj != DataF['Date'][i]):
+                v_D.append(DataF['Date'][i])
+                Dhwaj= DataF['Date'][i]
+        else:
+            DataF['Date'][i] = -1
+
+  
+    DataF = DataF.sort_values('Roll')
+
+    
